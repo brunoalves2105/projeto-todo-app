c4adabd4af5ad7775931828042213907fe3a4c96
bruno alves <bruno_2105@outlook.com>
2020-07-15 14:46:30 -0300

    criação do index.html

--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -9,32 +9,32 @@ module.exports = {
     },
     devServer: {
         port: 8080,
-        contentBase: './public'
+        contentBase: './public',
     },
-    resolve : {
-        extencions: ['', '.js', '.jsx'],
+    resolve: {
+        extensions: ['', '.js', '.jsx'],
         alias: {
             modules: __dirname + '/node_modules'
         }
     },
-    plugins: [
+    plugins: [ 
         new ExtractTextPlugin('app.css')
     ],
     module: {
         loaders: [{
             test: /.js[x]?$/,
-            loaders: 'babel-loader',
+            loader: 'babel-loader',
             exclude: /node_modules/,
             query: {
-                preset: ['es2015', 'react'],
+                presets: ['es2015', 'react'],
                 plugins: ['transform-object-rest-spread']
             }
         }, {
             test: /\.css$/,
             loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-    }, {
-        test: /\.woff|.woff2|.tff|.eat|.svg*.*$/,
-        loader: 'file'
-    }]
+        }, {
+            test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
+            loader: 'file'
+        }]
     }
 }
